cdfb8b628df466f8517acc767a0df35602a697a9
satel31 <blokhnina.tatiana@yandex.ru>
2023-03-10 00:56:32 +0300

    Made class Channel, changed tha API_KEY name in manual, made requirements file

--- helper/youtube_api_manual.py
+++ helper/youtube_api_manual.py
@@ -8,7 +8,7 @@ import isodate
 
 
 # YT_API_KEY скопирован из гугла и вставлен в переменные окружения
-api_key: str = os.getenv('YT_API_KEY')
+api_key: str = os.getenv('API_KEY')
 
 # создать специальный объект для работы с API
 youtube = build('youtube', 'v3', developerKey=api_key)
@@ -25,8 +25,8 @@ docs: https://developers.google.com/youtube/v3/docs/channels/list
 
 сервис для быстрого получения id канала: https://commentpicker.com/youtube-channel-id.php
 '''
-# channel_id = 'UCMCgOm8GZkHp8zJ6l7_hIuA'  # вДудь
-channel_id = 'UC1eFXmJNkjITxPFWTy6RsWg'  # Редакция
+channel_id = 'UCMCgOm8GZkHp8zJ6l7_hIuA'  # вДудь
+#channel_id = 'UC1eFXmJNkjITxPFWTy6RsWg'  # Редакция
 channel = youtube.channels().list(id=channel_id, part='snippet,statistics').execute()
 printj(channel)
 
@@ -53,7 +53,7 @@ docs: https://developers.google.com/youtube/v3/docs/playlistItems/list
 https://www.youtube.com/playlist?list=PLguYHBi01DWr4bRWc4uaguASmo7lW4GCb
 или из ответа API: см. playlists выше
 '''
-# playlist_id = 'PLguYHBi01DWrlpOkXwOYe8qjGFyqobcoO'
+#playlist_id = 'PLguYHBi01DWrlpOkXwOYe8qjGFyqobcoO'
 playlist_id = 'PLguYHBi01DWr4bRWc4uaguASmo7lW4GCb'
 playlist_videos = youtube.playlistItems().list(playlistId=playlist_id,
                                                part='contentDetails',
--- homework-1/main.py
+++ homework-1/main.py
@@ -4,6 +4,8 @@ if __name__ == '__main__':
     vdud = Channel('UCMCgOm8GZkHp8zJ6l7_hIuA')
     vdud.print_info()
 
+
+
     """
 {
   "kind": "youtube#channelListResponse",
